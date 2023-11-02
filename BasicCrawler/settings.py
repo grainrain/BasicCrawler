1a8fcd162f0e7fcf27833f780000e683f2bdbe5f
thinkspace <igrainrain@163.com>
2023-11-02 14:34:49 +0800

    update commit

--- BasicCrawler/settings.py
+++ BasicCrawler/settings.py
@@ -62,7 +62,10 @@ DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
     'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
     'scrapy_fake_useragent.middleware.RetryUserAgentMiddleware': 401,
-    'BasicCrawler.middlewares.BasiccrawlerDownloaderMiddleware': 543
+    # 'BasicCrawler.middlewares.BasiccrawlerDownloaderMiddleware': 543,
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
 }
 
 FAKEUSERAGENT_PROVIDERS = [
@@ -118,8 +121,7 @@ COMPRESSION_ENABLED = False
 
 
 
-SPLASH_URL = 'http://192.168.59.103:8050'
-DOWNLOADER_MIDDLEWARES = {
-    'scrapyjs.SplashMiddleware': 725,
-}
-DUPEFILTER_CLASS = 'scrapyjs.SplashAwareDupeFilter'
+SPIDER_MIDDLEWARES = {'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,}
+SPLASH_URL = 'http://localhost:8050'
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
--- BasicCrawler/spiders/BasicCrawlerSpider.py
+++ BasicCrawler/spiders/BasicCrawlerSpider.py
@@ -1,37 +1,26 @@
 import scrapy
+from scrapy_splash import SplashRequest
 
 
 class BasiccrawlerspiderSpider(scrapy.Spider):
     name = "BasicCrawlerSpider"
     allowed_domains = ["zaobao.com"]
-
     # start_urls = ["https://www.zaobao.com/realtime/china"]
 
     def start_requests(self):
         urls = [
-            'https://www.zaobao.com/realtime/china'
+            'https://newiic.jaas.ac.cn/xww/tzgg/index.html'
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-            # yield scrapy.Request(url=url, callback=self.parse, meta={'url': url}, dont_filter=True)
+            yield SplashRequest(url=url,args={'wait': 5,  'timeout': 90}, dont_filter=True)
 
     def parse(self, response):
-        print(20 * "-", '热点新闻', 20 * "-")
-        hot_news_list = response.xpath("//div[@id='main-container']/div[contains(@class,'float-lg-right')]//a")
-        for news in hot_news_list:
-            title = news.xpath("./@title").extract_first()
-            url = news.xpath("./@href").extract_first()
-            url = response.urljoin(url)
-            print(title, '/', url)
-        pass
-
-
         print(20 * "-", '新闻列表', 20 * "-")
-        news_list = response.xpath("//div[@id='main-container']/div[contains(@class,'float-lg-left')]//a")
+        news_list = response.xpath("//*[@id='当前栏目list']/div[1]/ul/li")
         for news in news_list:
-            title = news.xpath("./div[@class='flex-1']/div[1]/text()").extract_first()
-            publish_date = news.xpath("./div[@class='flex-1']/div[2]/text()").extract_first()
-            url = news.xpath("./@href").extract_first()
+            title = news.xpath("./label[1]/a/@title").extract_first()
+            publish_date = news.xpath("./label[2]/text()").extract_first()
+            url = news.xpath("./label[1]/a/@href").extract_first()
             url = response.urljoin(url)
             print(title, '/', publish_date, '/', url)
         pass
